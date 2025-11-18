3bde38580d60282616a65df5ef727fcd716605e9
MonishB123 <103612357+MonishB123@users.noreply.github.com>
2025-11-18 16:49:02 -0500

    Update package description and license in setup.py

--- setup.py
+++ setup.py
@@ -10,16 +10,22 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/robot.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ufset',
     maintainer_email='mannyjainlakes@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Core robot nodes (lidar, imu, motors, tf broadcaster)',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            # Each node must have a main() function
+            'lidar_node = robot_core.lidar_node:main',
+            'imu_node = robot_core.imu_node:main',
+            'motor_node = robot_core.motor_node:main',
+            'tf_broadcaster = robot_core.tf_broadcaster:main',
         ],
     },
 )
